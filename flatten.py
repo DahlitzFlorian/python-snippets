6d58d42d5da492b527634eb7bf44eeed0f681022
florian dahlitz <f2dahlitz@freenet.de>
2019-01-23 22:38:12 +0100

    refactoring: blackification

--- flatten.py
+++ flatten.py
@@ -3,7 +3,9 @@ def flatten(input_arr, output_arr=None):
         output_arr = []
     for ele in input_arr:
         if isinstance(ele, Iterable):
-            flatten(ele, output_arr)    #tail-recursion
+            flatten(ele, output_arr)  # tail-recursion
         else:
-            output_arr.append(ele)      #produce the result
+            output_arr.append(ele)  # produce the result
+
+
 return output_arr
--- world_bank_data.py
+++ world_bank_data.py
@@ -4,16 +4,15 @@
 import csv, io, requests, zipfile  # noqa
 
 
-url = 'http://databank.worldbank.org/data/download/WDI_csv.zip'
-filename = 'WDIData.csv'
+url = "http://databank.worldbank.org/data/download/WDI_csv.zip"
+filename = "WDIData.csv"
 
 # Warning: this can take two minutes to download!!
 with zipfile.ZipFile(io.BytesIO(requests.get(url).content)) as zip_file:
-    print('\n'.join(name for name in zip_file.namelist()))
+    print("\n".join(name for name in zip_file.namelist()))
     zip_file.extractall()
 
 # On Python 2, remove: ", newline=''"
-with open(filename, newline='') as in_file:
+with open(filename, newline="") as in_file:
     for row in csv.reader(in_file):
-        print(', '.join(row))
-
+        print(", ".join(row))
