7dc90caadb3bc811e53218484b370ec40deb5103
Florian Dahlitz <f2dahlitz@freenet.de>
2019-03-24 11:23:05 +0100

    refactoring: remove class description as it will be part of the ebook

--- standard_lib/bitmask.py
+++ standard_lib/bitmask.py
@@ -1,12 +1,5 @@
 #! /bin/env python3
 
-"""Class that represents a bit mask.
-It has methods representing all the bitwise operations plus some
-additional features. The methods return a new BitMask object or
-a boolean result. See the bits module for more on the operations
-provided.
-"""
-
 
 class BitMask(int):
     def AND(self, bm):
