210b6e2fab7be00d2e23a5aa4432103bf1c89d9f
Florian Dahlitz <f2dahlitz@freenet.de>
2019-03-24 16:24:16 +0100

    refactoring(parse_query_string): explicitly import parse_qs instead of urllib

--- standard_lib/parse_query_string.py
+++ standard_lib/parse_query_string.py
@@ -1,6 +1,6 @@
-import urllib
+from urllib.parse import parse_qs
 
 
 input_string = "host=171.25.2.7&port=8080&port=8081"
-result = urllib.parse.parse_qs(input_string)
+result = parse_qs(input_string)
 print(result)
