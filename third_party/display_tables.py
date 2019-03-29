f9199c5eebfc0b941960d18b358c8f950f22a5e5
Florian Dahlitz <f2dahlitz@freenet.de>
2019-03-29 16:33:33 +0100

    refactoring(display_tables): add whitespaces to be PEP8 compliant

--- third_party/display_tables.py
+++ third_party/display_tables.py
@@ -1,7 +1,9 @@
 from prettytable import PrettyTable
 
+
 table = PrettyTable(["food", "price"])
 table.add_row(["ham", "$2"])
 table.add_row(["eggs", "$1"])
 table.add_row(["spam", "$4"])
+
 print(table)
